264b88f1e3d0882725b160362a35859d136732f1
Sebastien Wieckowski <sbwiecko@free.fr>
2020-06-22 21:20:38 +0200

    ex022 completed

--- exr-021_creer-structure-dossiers/01-sources/structure_dossier.py
+++ exr-021_creer-structure-dossiers/01-sources/structure_dossier.py
@@ -1,13 +1,23 @@
-chemin = "/Users/thibh/dossier_test"
+# chemin = "/Users/thibh/dossier_test"
+import os
 
-d = {"Films": ["Le seigneur des anneaux",
-			   "Harry Potter",
-			   "Moon",
-			   "Forrest Gump"],
-	 "Employes": ["Paul",
-	 		      "Pierre",
-				  "Marie"],
-	 "Exercices": ["les_variables",
-	 			   "les_fichiers",
-				   "les_boucles"]}
-           
+# Itérez sur le dictionnaire 'd' afin de créer les dossiers 'Films', 'Employes' et 'Exercices' ainsi que leur sous-dossier.
+
+d = {"Films":
+		["Le seigneur des anneaux",
+		"Harry Potter",
+		"Moon",
+		"Forrest Gump"],
+	 "Employes":
+	 	["Paul",
+	 	"Pierre",
+		"Marie"],
+	 "Exercices":
+	 	["les_variables",
+	 	"les_fichiers",
+		"les_boucles"]}
+
+path = ".\\"
+
+list_paths = [os.path.join(path, supfolder, folder) for supfolder in d for folder in d[supfolder]]
+for path in list_paths: os.makedirs(path, exist_ok=True)
--- exr-022_chercher-informations-fichier/01-sources/chercher_un_mot.py
+++ exr-022_chercher-informations-fichier/01-sources/chercher_un_mot.py
@@ -1,4 +1,27 @@
+"""
+1. Parcourir chaque fichier et récupérer le numéro du compte bancaire 'Crédit Mutuel' ainsi que le numéro de sécurité sociale.
+2. Afficher les deux numéros à la fin du script.
+"""
 import glob
+import json
 
-dossier = "/Users/thibh/formation-developpeur-python/exercices/systeme_exploitation/chercher_un_mot/sources/dossier_exemple/**"
+dossier = "./dossier_exemple/**" # os.path.dirname(__file__)
 files = glob.glob(dossier, recursive=True)
+
+# init pour éviter les erreurs en fin de script
+numero_compte = None
+numero_secu = None
+
+for file in files:
+    if "comptes_bancaires" in file:
+        with open(file, 'r') as f:
+            data_dict = json.load(f)
+            numero_compte = data_dict.get("Credit Mutuel").get("Numero de compte")
+            # manque ici une vérification de l'existance de la clé
+            
+    if "securite_sociale" in file:
+        with open(file, 'r') as f:
+            numero_secu = int(f.read().split(':')[1])
+
+print(f"numéro de compte Crédit Mutuel : {numero_compte}")
+print(f"numéro de sécurité sociale : {numero_secu}")
